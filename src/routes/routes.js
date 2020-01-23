d2243edd18e87ed8d5660ca414aeb957b1400fb9
EsteveSegura <estevesegura.gir@gmail.com>
2020-01-23 18:16:07 +0100

    test ready

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/' , (req,res) => {
-    res.render('index.ejs')
-})
+    res.render('index.ejs');
+});
 
 router.get('/streaming' , (req,res) => {
-    res.render('streaming.ejs')
-})
+    res.render('streaming.ejs');
+});
 
 router.get('/list' , (req,res) => {
-    res.render('listposts.ejs')
-})
+    res.render('listposts.ejs');
+});
+
 module.exports = router;
